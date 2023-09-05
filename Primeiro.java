203111fe44784ec10520e484ae7c96ce0bc9c0c2
Fabio Rafael Dias Viana <102665846+Fabio2500@users.noreply.github.com>
2023-09-05 09:02:45 -0300

    Add files via upload

---
+++ Primeiro.java
@@ -0,0 +1,10 @@
+public class Primeiro{ 
+  public static void main(String[] args)throws Exception{ 
+   String nome;
+   System.out.println("digite seu nome:");
+   nome = JUtil.readString;
+   System.out.println(nome);
+
+  }
+
+}
